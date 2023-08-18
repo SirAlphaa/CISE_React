7417b8c0ecb2cd9e34963cbc7abd1dbcf9ddc354
SirAlphaa <captainscar117@gmail.com>
2023-08-18 14:38:17 +1200

    Created The files for step 8b

---
+++ cise_ass1a_worksheet4/frontend/src/pages/index.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+    return (
+        <div className="container">
+            <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+        </div>
+    );
+}
